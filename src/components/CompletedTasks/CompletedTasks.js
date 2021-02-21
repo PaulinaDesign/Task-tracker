fcea11abc6fa9acf008fd4249d36649f8483d281
Paulina <paulina.jancyte@gmail.com>
2021-02-21 14:50:09 +0200

    Added CompletedTask component

--- src/components/CompletedTasks/CompletedTasks.js
+++ src/components/CompletedTasks/CompletedTasks.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import classNames from "classnames";
-import { Button, Task } from "../../components";
+import { Button, CompletedTask } from "../../components";
 import { MdKeyboardArrowDown } from "react-icons/md";
 import "./CompletedTasks.scss";
 
@@ -41,7 +41,7 @@ const CompletedTasks = (props) => {
       <div className="completed-tasks">
         { showTasks && completedTasks.length > 0 &&
           completedTasks.map((task) => (
-            <Task
+            <CompletedTask
               key={task.id}
               task={task}
             />
--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,7 @@
 export { default as AddTask } from "./AddTask/AddTask";
 export { default as Button } from "./Button/Button";
 export { default as CompletedTasks } from "./CompletedTasks/CompletedTasks";
+export { default as CompletedTask } from "./CompletedTask/CompletedTask";
 export { default as Footer } from "./Footer/Footer";
 export { default as Header } from "./Header/Header";
 export { default as Task } from "./Task/Task";
